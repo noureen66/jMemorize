7d965a5ae61fb487006f6d672cbdc8dbb6bd6042
salmaUsamaa <105728467+salmaUsamaa@users.noreply.github.com>
2024-05-05 17:45:04 +0300

    Update ExtentProgressBar.java

--- src/jmemorize/gui/swing/widgets/ExtentProgressBar.java
+++ src/jmemorize/gui/swing/widgets/ExtentProgressBar.java
@@ -15,49 +15,62 @@ import javax.swing.JProgressBar;
 /**
  * @author bret5 Copyright(C) 2007 bret5
  * 
- * This class extends JProgressBar in order to show two values for progress,
- * which are presumably related so that they can be shown on a stacked bar.
+ *         This class extends JProgressBar in order to show two values for
+ *         progress,
+ *         which are presumably related so that they can be shown on a stacked
+ *         bar.
  * 
- * The underlying data object, the BoundedRangeModel, already has a data member,
- * extent, representing the length of an inner range, so all we have to do is
- * expose methods for adjusting the value of extent and implement a paint method
- * which paints the inner range in a separate color.
+ *         The underlying data object, the BoundedRangeModel, already has a data
+ *         member,
+ *         extent, representing the length of an inner range, so all we have to
+ *         do is
+ *         expose methods for adjusting the value of extent and implement a
+ *         paint method
+ *         which paints the inner range in a separate color.
  * 
- * The drawback to this painting implementation is that if isStringPainted, then
- * the rectangle representing the extent paints over the text. - Workaround - if
- * you want the the text to be visible through the extent rectangle, set the
- * extentForeground to a partially transparent color. See the included main for
- * an example.
+ *         The drawback to this painting implementation is that if
+ *         isStringPainted, then
+ *         the rectangle representing the extent paints over the text. -
+ *         Workaround - if
+ *         you want the the text to be visible through the extent rectangle, set
+ *         the
+ *         extentForeground to a partially transparent color. See the included
+ *         main for
+ *         an example.
  * 
- * TODO - fixing this requires fully implementing paint (background, both
- * rectangles, and the text in three colors).
+ *         TODO - fixing this requires fully implementing paint (background,
+ *         both
+ *         rectangles, and the text in three colors).
  * 
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 1, or (at your option) any later version.
+ *         This program is free software; you can redistribute it and/or modify
+ *         it under
+ *         the terms of the GNU General Public License as published by the Free
+ *         Software
+ *         Foundation; either version 1, or (at your option) any later version.
  * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
+ *         This program is distributed in the hope that it will be useful, but
+ *         WITHOUT
+ *         ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *         FITNESS
+ *         FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ *         details.
  * 
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 675 Mass
- * Ave, Cambridge, MA 02139, USA.
+ *         You should have received a copy of the GNU General Public License
+ *         along with
+ *         this program; if not, write to the Free Software Foundation, Inc.,
+ *         675 Mass
+ *         Ave, Cambridge, MA 02139, USA.
  */
-public class ExtentProgressBar extends JProgressBar
-{
+public class ExtentProgressBar extends JProgressBar {
 
     private Color extentForeground;
 
-    public void setExtent(int n)
-    {
+    public void setExtent(int n) {
         getModel().setExtent(n);
         repaint();
     }
 
-    public int getExtent()
-    {
+    public int getExtent() {
         return getModel().getExtent();
     }
 
@@ -66,8 +79,7 @@ public class ExtentProgressBar extends JProgressBar
      * 
      * @param fg
      */
-    public void setExtentForeground(Color fg)
-    {
+    public void setExtentForeground(Color fg) {
         extentForeground = fg;
     }
 
@@ -76,13 +88,11 @@ public class ExtentProgressBar extends JProgressBar
      * 
      * @return the color used to paint the extent value
      */
-    public Color getExtentForeground()
-    {
+    public Color getExtentForeground() {
         return extentForeground;
     }
 
-    protected void paintComponent(Graphics g)
-    {
+    protected void paintComponent(Graphics g) {
         // first use the superclass to paint the bar
         super.paintComponent(g);
         Insets insets = getInsets();
@@ -100,19 +110,16 @@ public class ExtentProgressBar extends JProgressBar
         int y, w, h = 0;
         int barHeight = getHeight() - (insets.top + insets.bottom);
         int barWidth = getWidth() - (insets.left + insets.right);
-        if (getOrientation() == HORIZONTAL)
-        {
+        if (getOrientation() == HORIZONTAL) {
             y = getY() + insets.top;
-            x += (int)(barWidth * (value / outerRange));
-            w = (int)(barWidth * (extent / outerRange));
+            x += (int) (barWidth * (value / outerRange));
+            w = (int) (barWidth * (extent / outerRange));
             h = barHeight;
-        }
-        else
-        {
+        } else {
             // Vertical bars start at the bottom and go up
             y = getY() + barHeight;
-            y -= (int)(barHeight * (value / outerRange));
-            h = (int)(barHeight * (extent / outerRange));
+            y -= (int) (barHeight * (value / outerRange));
+            h = (int) (barHeight * (extent / outerRange));
             y -= h;
             w = barWidth;
         }
@@ -121,8 +128,7 @@ public class ExtentProgressBar extends JProgressBar
         g.setColor(oldColor);
     }
 
-    private static void createAndShowGUI()
-    {
+    private static void createAndShowGUI() {
         // Make sure we have nice window decorations.
         JFrame.setDefaultLookAndFeelDecorated(true);
 
@@ -147,29 +153,32 @@ public class ExtentProgressBar extends JProgressBar
 
         frame.getContentPane().add(bar);
         frame.addKeyListener(new KeyAdapter()
+        /* BUG#6 FIXED SALMA */
         {
-            public void keyPressed(KeyEvent arg0)
-            {
-                switch (arg0.getKeyCode())
-                {
-                case KeyEvent.VK_LEFT:
-                    bar.setValue(bar.getValue() - 1);
-                    break;
-                case KeyEvent.VK_RIGHT:
-                    bar.setValue(bar.getValue() + 1);
-                    break;
-                case KeyEvent.VK_UP:
-                    bar.setExtent(bar.getExtent() + 1);
-                    break;
-                case KeyEvent.VK_DOWN:
-                    bar.setExtent(bar.getExtent() - 1);
-                    break;
-                case KeyEvent.VK_1:
-                    bar.setOrientation(JProgressBar.HORIZONTAL);
-                    break;
-                case KeyEvent.VK_2:
-                    bar.setOrientation(JProgressBar.VERTICAL);
-                    break;
+            public void keyPressed(KeyEvent arg0) {
+                switch (arg0.getKeyCode()) {
+                    case KeyEvent.VK_LEFT:
+                        bar.setValue(bar.getValue() - 1);
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        bar.setValue(bar.getValue() + 1);
+                        break;
+                    case KeyEvent.VK_UP:
+                        bar.setExtent(bar.getExtent() + 1);
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        bar.setExtent(bar.getExtent() - 1);
+                        break;
+                    case KeyEvent.VK_1:
+                        bar.setOrientation(JProgressBar.HORIZONTAL);
+                        break;
+                    case KeyEvent.VK_2:
+                        bar.setOrientation(JProgressBar.VERTICAL);
+                        break;
+                    default:
+                        // the default case here
+                        System.out.println("Unrecognized key code: " + arg0.getKeyCode());
+
                 }
             }
         });
@@ -179,14 +188,11 @@ public class ExtentProgressBar extends JProgressBar
         frame.setVisible(true);
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // Schedule a job for the event-dispatching thread:
         // creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable()
-        {
-            public void run()
-            {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
                 createAndShowGUI();
             }
         });
